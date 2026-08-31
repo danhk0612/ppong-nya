880f7be17294f4a924ac295f993447dab9ceb7f3
HAEKWANG DAN <danhk0612@gmail.com>
2026-08-31 11:54:18 +0900

    refactor: remove auth app types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,32 +1,5 @@
-import type { UserRole } from "@prisma/client";
-
 declare global {
-  namespace App {
-    interface Locals {
-      auth(): Promise<import("@auth/sveltekit").Session | null>;
-    }
-    interface PageData {
-      session?: import("@auth/sveltekit").Session | null;
-    }
-  }
-}
-
-declare module "@auth/sveltekit" {
-  interface Session {
-    user?: {
-      id: string;
-      role: UserRole;
-      passwordChangeRequired: boolean;
-      passwordChangeRequired: boolean;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
-  }
-
-  interface User {
-    role: UserRole;
-  }
+  namespace App {}
 }
 
 export {};
